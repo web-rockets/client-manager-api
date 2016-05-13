71c5f55fc1e336e810844eb91af26a7c3f4ce8ee
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-05-13 17:20:57 -0300

    added API routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  scope :api, defaults: { format: :json } do
+    get 'users' => 'users#all'
+    put 'users/:id' => 'users#update'
+    post 'users' => 'users#create'
+    delete 'users' => 'users#remove'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
