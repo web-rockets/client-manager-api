b93d2cd5479a8a1a469a2acfb37c618cbd65f59a
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2016-06-01 23:04:31 -0300

    adicionados seeds para o ambiente de testes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create name: 'Administrador', email: 'admin@gmail.com', password: '123456'
+
+case Rails.env
+when 'development'
+    puts 'Nada a alimentar'
+when 'test'
+    user = User.create name: 'John', email: 'john@gmail.com', password: '123456'
+    puts "#{user.name} criado com sucesso"
+    user = User.create name: 'Gib', email: 'gib@gmail.com', password: '123456'
+    puts "#{user.name} criado com sucesso"
+    user = User.create name: 'Joe', email: 'joe@gmail.com', password: '123456'
+    puts "#{user.name} criado com sucesso"
+    user = User.create name: 'Lucy', email: 'lucy@gmail.com', password: '123456'
+    puts "#{user.name} criado com sucesso"
+    user = User.create name: 'Anna', email: 'anna@gmail.com', password: '123456'
+    puts "#{user.name} criado com sucesso"
+end
